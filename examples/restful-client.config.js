08ab5d44ab40f0262cf27a7f6b5f9e52289683bb
Victor Bury <bury.victor@gmail.com>
2019-12-28 20:27:24 +0100

    fix(examples): update

--- examples/restful-client.config.js
+++ examples/restful-client.config.js
@@ -6,12 +6,12 @@ module.exports = {
   'petstore-file': {
     file: 'examples/petstore.yaml',
     output: 'examples/petstoreFromFileSpecWithConfig.tsx',
-    types: './model',
+    types: 'examples/model',
   },
   'petstore-file-transfomer': {
     file: 'examples/petstore.yaml',
     output: 'examples/petstoreFromFileSpecWithTransformer.tsx',
-    types: './model',
+    types: 'examples/model',
     transformer: 'examples/transformer-add-version.js',
   },
 };
