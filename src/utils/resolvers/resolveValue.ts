9238267e8d8c6f20eaeaeba8493803cf4231de29
Victor Bury <bury.victor@gmail.com>
2020-02-11 13:04:51 +0100

    fix(generator): handling enum integer

--- src/utils/resolvers/resolveValue.ts
+++ src/utils/resolvers/resolveValue.ts
@@ -1,7 +1,7 @@
-import { SchemaObject } from 'openapi3-ts';
-import { getRef } from '../getters/getRef';
-import { getScalar } from '../getters/getScalar';
-import { isReference } from '../isReference';
+import {SchemaObject} from 'openapi3-ts';
+import {getRef} from '../getters/getRef';
+import {getScalar} from '../getters/getScalar';
+import {isReference} from '../isReference';
 
 /**
  * Resolve the value of a schema object to a proper type definition.
@@ -10,7 +10,7 @@ import { isReference } from '../isReference';
 export const resolveValue = (schema: SchemaObject) => {
   if (isReference(schema)) {
     const value = getRef(schema.$ref);
-    return { value, imports: [value] };
+    return {value, imports: [value], type: 'object'};
   }
 
   return getScalar(schema);
