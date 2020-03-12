1f53dd2ffa2026fb55fbddb703de8970a63a8eae
Sam <sverps@gmail.com>
2020-03-12 06:34:36 +0100

    fix(generator): pathOnly generation with only 1 element in imports array now works (#20)

--- src/utils/generators/generateImports.ts
+++ src/utils/generators/generateImports.ts
@@ -2,16 +2,7 @@ import { camel } from "case";
 
 export const generateImports = (imports: string[] = [], path: string = '.', pathOnly: boolean = false) => {
   if (pathOnly) {
-    return imports.sort().reduce((acc, imp, index) => {
-      if (!acc) {
-        return `import { ${imp},\n`;
-      }
-      if (imports.length - 1 === index) {
-        return acc + `  ${imp},\n} from \'${path}\'; \n`;
-      }
-
-      return acc + `  ${imp},\n`;
-    }, '');
+    return `import {\n  ${imports.sort().join(',\n  ')}\n} from \'${path}\';\n`;
   }
-  return imports.sort().reduce((acc, imp) => acc + `import { ${imp} } from \'${path}/${camel(imp)}\'; \n`, '');
+  return imports.sort().map((imp) => `import { ${imp} } from \'${path}/${camel(imp)}\';\n`).join('');
 };
