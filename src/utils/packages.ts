8229fda2c2b3bc9f67f033b618b42fb4df4fbcd4
Antoine <antoine.paques@gmail.com>
2020-03-25 17:26:58 +0100

    feat(logs): centralizes all system logs (#26)
    #8

--- src/utils/packages.ts
+++ src/utils/packages.ts
@@ -1,5 +1,22 @@
 import {readFileSync} from 'fs';
 import {join} from 'path';
 
+export type PackageJson = {
+  name: string;
+  version: string;
+  description?: string;
+  license?: string;
+  author?: {
+    name: string;
+    email: string;
+  };
+  repository?: {
+    type: string;
+    url: string;
+  };
+};
+
 export const getPackage = () =>
-  JSON.parse(readFileSync(join(__dirname, '../../package.json'), 'utf-8'));
+  JSON.parse(
+    readFileSync(join(__dirname, '../../package.json'), 'utf-8')
+  ) as PackageJson;
