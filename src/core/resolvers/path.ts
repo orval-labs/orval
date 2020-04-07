831aa2a78c27c809a1ac346d7a2ad5fb80352081
Victor Bury <bury.victor@gmail.com>
2020-04-07 09:51:55 +0200

    fix(resolver): path

--- src/core/resolvers/path.ts
+++ src/core/resolvers/path.ts
@@ -1,8 +1,9 @@
+import { resolve } from 'path';
 const segmentize = (uri: string) => uri.replace(/(^\/+|\/+$)/g, '').split('/');
 
 export const resolvePath = (base: string, to: string) => {
-  const toSegmentize = segmentize(to);
-  const baseSegmentize = segmentize(base);
+  const toSegmentize = segmentize(resolve(to));
+  const baseSegmentize = segmentize(resolve(base));
 
   return toSegmentize.reduce((acc: string, path: string, index: number) => {
     if (path === baseSegmentize[index]) {
