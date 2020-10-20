020dd2a7644cc15d13826c4e38c34715ac9ed5d1
anymaniax <bury.victor@gmail.com>
2020-10-20 13:33:14 +0200

    fix(mutator): correctly handle blob body

--- src/core/generators/mutator.ts
+++ src/core/generators/mutator.ts
@@ -17,9 +17,13 @@ export const generateMutator = ({
   }
 
   const type = `(url: string,${
-    body.definition ? ` data: ${body.definition}, ` : ''
+    body.definition
+      ? ` data: ${body.isBlob ? `FormData` : body.definition}, `
+      : ''
   } config?: object) => [string, ${
-    body.definition ? `${body.definition} | undefined, ` : ''
+    body.definition
+      ? `${body.isBlob ? `FormData` : body.definition} | undefined, `
+      : ''
   } object | undefined]`;
 
   const mutatorFn = isString(mutator)
