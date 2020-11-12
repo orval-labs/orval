2c07b8812c4932dc3fe3e980b7c11f3b9787fb6e
anymaniax <bury.victor@gmail.com>
2020-11-12 14:44:55 +0100

    fix(docs): remove useless script

--- docs/src/pages/_app.js
+++ docs/src/pages/_app.js
@@ -4,27 +4,7 @@ import Head from 'next/head';
 import React from 'react';
 import '../styles/index.css';
 
-function loadScript(src, attrs = {}) {
-  if (typeof document !== 'undefined') {
-    const script = document.createElement('script');
-    script.async = true;
-    script.defer = true;
-    Object.keys(attrs).forEach((attr) =>
-      script.setAttribute(attr, attrs[attr]),
-    );
-    script.src = src;
-    document.body.appendChild(script);
-  }
-}
-
 function MyApp({ Component, pageProps }) {
-  React.useEffect(() => {
-    loadScript('https://buttons.github.io/buttons.js');
-    loadScript('https://tanstack.ck.page/e394781e7a/index.js', {
-      'data-uid': 'e394781e7a',
-    });
-  }, []);
-
   return (
     <>
       <Head>
@@ -49,8 +29,6 @@ function MyApp({ Component, pageProps }) {
           }}
         />
       </Head>
-      {/* eslint-disable-next-line jsx-a11y/alt-text */}
-      <img src="https://static.scarf.sh/a.png?x-pxid=c03d3ddd-b47e-4e26-a9b2-9df68b2ac970" />
       <SearchProvider>
         <Component {...pageProps} />
       </SearchProvider>
