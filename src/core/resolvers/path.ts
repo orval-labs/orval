7bd01ab27b904a6946a07435cb3abfe96f2eb58f
anymaniax <bury.victor@gmail.com>
2020-12-09 20:36:49 +0100

    fix(resolvers): path handle correctly folder base

--- src/core/resolvers/path.ts
+++ src/core/resolvers/path.ts
@@ -1,9 +1,11 @@
 import { resolve, sep } from 'path';
+import { getFileInfo } from '../../utils/file';
 const segmentize = (uri: string) => uri.split(sep);
 
 export const resolvePath = (base: string, to: string) => {
+  const baseInfo = getFileInfo(resolve(base));
   const toSegmentize = segmentize(resolve(to));
-  const baseSegmentize = segmentize(resolve(base));
+  const baseSegmentize = segmentize(baseInfo.path);
 
   return toSegmentize.reduce((acc: string, path: string, index: number) => {
     if (path === baseSegmentize[index]) {
