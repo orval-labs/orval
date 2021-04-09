a35ab7e3cbf9534501b16bb8d667d7181a90de1f
Christoph von Krüchten <cvk@treestack.de>
2021-04-09 10:02:56 +0200

    fix(input): allow simple hostname

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -17,7 +17,7 @@ export const generalJSTypesWithArray = generalJSTypes.reduce<string[]>(
 
 export const VERBS_WITH_BODY = [Verbs.POST, Verbs.PUT, Verbs.PATCH];
 
-export const URL_REGEX = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/;
+export const URL_REGEX = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)*[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/;
 
 export const SPECIAL_CHAR_REGEX = /[^\w\s]/g;
 
