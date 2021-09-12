fc8e65969c8741276cb664fdd25f5cb79a8cedd1
anymaniax <bury.victor@gmail.com>
2021-09-13 00:13:46 +0200

    feat(cli): move from commander to cac

--- src/bin/orval.ts
+++ src/bin/orval.ts
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import { Command } from 'commander';
+import { cac } from 'cac';
 import pkg from '../../package.json';
 import { generateConfig, generateSpec } from '../generate';
 import { isString } from '../utils/is';
@@ -7,7 +7,7 @@ import { startMessage } from '../utils/messages/logs';
 import { normalizeOptions } from '../utils/options';
 import { startWatcher } from '../utils/watcher';
 
-const program = new Command();
+const cli = cac('orval');
 
 startMessage({
   name: pkg.name,
@@ -15,11 +15,12 @@ startMessage({
   description: pkg.description,
 });
 
-program.version(pkg.version);
+cli.version(pkg.version);
 
-program
-  .command('default [open-api-file]', { isDefault: true, hidden: true })
-  .description('generate orval type-safe from OpenAPI specs')
+cli
+  .command('[config]', 'generate client with appropriate type-signatures from OpenAPI specs', {
+    ignoreOptionDefaultValue: true,
+  })
   .option('-o, --output <path>', 'output file destination')
   .option('-i, --input <path>', 'input file (yaml or json openapi specs)')
   .option('-c, --config <path>', 'override flags by a config file')
@@ -54,4 +55,6 @@ program
     }
   });
 
-program.parse(process.argv);
+cli.help();
+
+cli.parse(process.argv);
