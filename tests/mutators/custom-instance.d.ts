10d18e4c56eb8342e24554a8b2915ae454841a87
Victor <bury.victor@gmail.com>
2021-10-08 14:46:49 +0200

    test(generates): first version (#229)

---
+++ tests/mutators/custom-instance.d.ts
@@ -0,0 +1,5 @@
+import { AxiosRequestConfig } from 'axios';
+export declare const AXIOS_INSTANCE: import("axios").AxiosInstance;
+export declare const customInstance: <T>(config: AxiosRequestConfig) => Promise<T>;
+export default customInstance;
+//# sourceMappingURL=custom-instance.d.ts.map
