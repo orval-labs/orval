21d03347e48807c122f1a668a31bced3af78c3e9
Olzhas Alexandrov <olzhas@alexandrov.co>
2022-04-28 10:38:49 -0500

    feat: export constants & fix formatting

--- src/index.ts
+++ src/index.ts
@@ -52,12 +52,13 @@ const generate = async (
   }
 };
 
+export * from './constants';
 export * from './types/generator';
-export * from "./core/generators/imports"
-export * from "./core/generators/options"
-export * from "./utils/tsconfig"
-export * from "./utils/string"
-export * from "./utils/case"
+export * from './core/generators/imports';
+export * from './core/generators/options';
+export * from './utils/tsconfig';
+export * from './utils/string';
+export * from './utils/case';
 export { defineConfig };
 export { Options };
 export { generate };
