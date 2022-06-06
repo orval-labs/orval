5249df73f188c5bda3f5014c4f016ad79e1794c0
Robert Jackson <Kangarused@users.noreply.github.com>
2022-06-06 15:05:10 +0930

    Generate constant enum name specifier
    Update the getEnum function to also generate a constant reference to the name of the enum, the constant is given the variable: '<enumName>Enum' and its value is the name of the enum, for example; an enum called SystemCodes would generate - `SystemCodesEnum = 'SystemCodes';`. This provides a means of accessing the enum name on the client side in a type-safe manner, ie. if the models change your client side code will throw an error because SystemCodesEnum is missing.

--- src/core/getters/enum.ts
+++ src/core/getters/enum.ts
@@ -8,6 +8,10 @@ export const getEnum = (value: string, type: string, enumName: string) => {
 
   enumValue += `\n\n`;
 
+  enumValue += `export const ${enumName}Enum = '${enumName}';`;
+  
+  enumValue += `\n\n`;
+  
   enumValue += '// eslint-disable-next-line @typescript-eslint/no-redeclare\n';
 
   enumValue += `export const ${enumName} = {\n${implementation}} as const;\n`;
