83d1d59323d69dca76cefd7d5c5f03533e902233
anymaniax <victor@emaniax.io>
2022-11-29 16:51:35 +0100

    fix(core): getters scalar stringify all enum items

--- packages/core/src/getters/scalar.ts
+++ packages/core/src/getters/scalar.ts
@@ -1,8 +1,6 @@
 import { SchemaObject } from 'openapi3-ts';
-import { ContextSpecs } from '../types';
-import { ResolverValue } from '../types';
-import { isString } from '../utils';
-import { escape } from '../utils';
+import { ContextSpecs, ResolverValue } from '../types';
+import { escape, isString } from '../utils';
 import { getArray } from './array';
 import { getObject } from './object';
 
@@ -77,10 +75,11 @@ export const getScalar = ({
       if (item.enum) {
         value = `'${item.enum
           .map((enumItem: string) =>
-            isString(enumItem) ? escape(enumItem) : enumItem,
+            isString(enumItem) ? escape(enumItem) : `${enumItem}`,
           )
           .filter(Boolean)
           .join(`' | '`)}'`;
+
         isEnum = true;
       }
 
