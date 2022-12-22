128d0c4413b9a672fe585851037104bed85d5503
anymaniax <victor@emaniax.io>
2022-12-22 11:35:43 +0100

    fix(msw): handling combined type array correctly

--- packages/msw/src/getters/combine.ts
+++ packages/msw/src/getters/combine.ts
@@ -97,7 +97,11 @@ export const combineSchemasMock = ({
       !combine || (combine.separator === 'oneOf' && separator === 'allOf');
 
     if (!index && isObjectBounds) {
-      if (resolvedValue.enums || separator === 'oneOf') {
+      if (
+        resolvedValue.enums ||
+        separator === 'oneOf' ||
+        resolvedValue.type === 'array'
+      ) {
         if (arr.length === 1) {
           return `faker.helpers.arrayElement([${currentValue}])`;
         }
@@ -115,7 +119,11 @@ export const combineSchemasMock = ({
     }
 
     if (isLastElement) {
-      if (resolvedValue.enums || separator === 'oneOf') {
+      if (
+        resolvedValue.enums ||
+        separator === 'oneOf' ||
+        resolvedValue.type === 'array'
+      ) {
         return `${acc}${currentValue}${!combine ? '])' : ''}`;
       }
 
