eb0ed3b467453ed60d54508c33be44fbd292664c
anymaniax <victor@emaniax.io>
2023-03-05 13:50:18 +0100

    docs(discord): add link

--- docs/src/siteConfig.js
+++ docs/src/siteConfig.js
@@ -3,6 +3,7 @@ export const siteConfig = {
   editUrl: 'https://github.com/anymaniax/orval/edit/master/docs/src/pages',
   copyright: `Copyright © ${new Date().getFullYear()} Victor Bury. All Rights Reserved.`,
   repoUrl: 'https://github.com/anymaniax/orval',
+  discordUrl: 'https://discord.gg/6fC2sjDU7w',
   algolia: {
     appId: process.env.NEXT_PUBLIC_ALGOLIA_APP_ID,
     apiKey: process.env.NEXT_PUBLIC_ALGOLIA_API_KEY,
