8f681a5e8edd553e3b51b2ad2e38e11dba562f7b
Glade <gladekettle@gmail.com>
2023-05-07 00:25:21 +1000

    feat(samples): improve vue query sample and use param

---
+++ samples/vue-query/src/components/pet.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="pet" v-if="pet && petId" :key="pet.id">
+    {{ petId }}
+    {{ pet }}
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { computed, unref } from 'vue';
+import { useShowPetById } from '../api/endpoints/petstoreFromFileSpecWithTransformer';
+
+const props = defineProps<{ petId: string }>();
+const petId = computed(() => props.petId);
+const petQuery = useShowPetById(petId); // @todo fix this typing
+const pet = computed(() => unref(petQuery?.data));
+</script>
+
+<style scoped>
+.pet {
+  padding: 2rem;
+  background-color: #c0c2;
+}
+</style>
--- samples/vue-query/src/main.ts
+++ samples/vue-query/src/main.ts
@@ -1,9 +1,12 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import { VueQueryPlugin } from '@tanstack/vue-query';
 
 // NEW
 if (process.env.NODE_ENV === 'development') {
   import('./mock');
 }
 
-createApp(App).mount('#app');
+const app = createApp(App);
+app.use(VueQueryPlugin);
+app.mount('#app');
