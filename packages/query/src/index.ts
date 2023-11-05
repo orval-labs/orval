6fce8d7bf59319968494e3772e5d66914e58f20f
antongeorgiev <byghoster@gmail.com>
2023-11-05 19:06:11 +0300

    feat(query): prefetch functions fix pageParam

--- packages/query/src/index.ts
+++ packages/query/src/index.ts
@@ -848,7 +848,7 @@ const generateQueryImplementation = ({
           )
             return param.destructured;
           return param.name === 'params'
-            ? `{...params, ${queryParam}: pageParam || params.page, }`
+            ? `{...params, ${queryParam}: pageParam }`
             : param.name;
         })
         .join(',')
