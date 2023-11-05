5614333402aa1ef4b2fcacf806f1f2a454a58b96
Melloware <mellowaredev@gmail.com>
2023-11-05 11:16:21 -0500

    Update case.test.ts

--- packages/core/src/utils/case.test.ts
+++ packages/core/src/utils/case.test.ts
@@ -27,20 +27,22 @@ describe('pascal case testing', () => {
     expect(pascal('foo_bar_API')).toBe('FooBarAPI');
   });
 });
-    
+
 describe('kebab-case a few examples', () => {
-  //fix #937, issue #936, results in kebab routine being potentially called
-  //on a string **repeatedly**.
-  //Do some basic kebab case checks
-  //Additionally, test that the kebab routine is Idempotent
-  [
-    ['Pet', 'pet'],
-    ['pet', 'pet'],
-    ['PetTag', 'pet-tag'],
-    ['pet-tag', 'pet-tag'],
-    ['PetTagWithFourWords', 'pet-tag-with-four-words'],
-    ['pet-tag-with-four-words', 'pet-tag-with-four-words'],
-  ].forEach(([input, expected]) => {
-    it(`should process ${input} to ${expected}`, () => {
-      expect(kebab(input)).toBe(expected);
- });
+    //fix #937, issue #936, results in kebab routine being potentially called
+    //on a string **repeatedly**.
+    //Do some basic kebab case checks
+    //Additionally, test that the kebab routine is Idempotent
+    [
+        ['Pet', 'pet'],
+        ['pet', 'pet'],
+        ['PetTag', 'pet-tag'],
+        ['pet-tag', 'pet-tag'],
+        ['PetTagWithFourWords', 'pet-tag-with-four-words'],
+        ['pet-tag-with-four-words', 'pet-tag-with-four-words'],
+    ].forEach(([input, expected]) => {
+        it(`should process ${input} to ${expected}`, () => {
+            expect(kebab(input)).toBe(expected);
+        });
+    });
+});
