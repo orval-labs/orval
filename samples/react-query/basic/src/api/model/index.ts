ed6204113dfeeab1e916d7efcff883deea8cc798
hallucinogenizer <tech@apollo-group.io>
2024-05-29 01:30:23 +0500

    chore: removed redundant files from react-query/basic output folders

--- samples/react-query/basic/src/api/model/index.ts
+++ samples/react-query/basic/src/api/model/index.ts
@@ -5,22 +5,12 @@
  * OpenAPI spec version: 1.0.0
  */
 
-export * from './cat';
-export * from './catType';
 export * from './createPetsBody';
-export * from './dachshund';
-export * from './dachshundBreed';
-export * from './dog';
-export * from './dogType';
 export * from './error';
-export * from './labradoodle';
-export * from './labradoodleBreed';
 export * from './listPetsNestedArrayParams';
 export * from './listPetsParams';
-export * from './listPetsRootArrayParams';
 export * from './pet';
 export * from './petCallingCode';
 export * from './petCountry';
-export * from './pets';
 export * from './petsArray';
 export * from './petsNestedArray';
