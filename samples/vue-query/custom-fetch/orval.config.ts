a1dcfbc4c9654885b485bbe342b967d1806d1e14
soartec-lab <info@soartec-lab.work>
2024-07-14 03:06:41 +0000

    chore: make `vue-query-custom-fetch` sample

---
+++ samples/vue-query/custom-fetch/orval.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'orval';
+
+export default defineConfig({
+  petstore: {
+    output: {
+      mode: 'tags-split',
+      target: 'src/gen/petstore.ts',
+      schemas: 'src/gen/models',
+      client: 'vue-query',
+      httpClient: 'fetch',
+      baseUrl: 'http://localhost:8000',
+      mock: true,
+      prettier: true,
+      override: {
+        mutator: {
+          path: './src/custom-fetch.ts',
+          name: 'customFetch',
+        },
+      },
+    },
+    input: {
+      target: './petstore.yaml',
+    },
+  },
+});
