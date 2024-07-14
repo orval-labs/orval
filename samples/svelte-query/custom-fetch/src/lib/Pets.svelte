2a34bca6106fed739e4e2aea1c147b0ee5f9971e
soartec-lab <info@soartec-lab.work>
2024-07-14 04:02:18 +0000

    chore: add `svelte-query-custom-fetch` sample

---
+++ samples/svelte-query/custom-fetch/src/lib/Pets.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { createListPets, createCreatePets } from "../gen/pets/pets";
+
+  const queryResult = createListPets();
+  const mutation = createCreatePets();
+  
+</script>
+
+<h2>Pets</h2>
+
+{#if $queryResult.data}
+  <ul>
+    {#each $queryResult.data.data as pet}
+      <li>{pet.name}</li>
+    {/each}
+  </ul>
+
+  <button
+    on:click={() => {
+      $mutation.mutate({ data: [{ name: 'testName', tag: 'testTag' }]})
+    }}>
+    Create
+  </button>
+{/if}
