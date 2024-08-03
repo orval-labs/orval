44ac828e7ea4441fe946a948fbb467693ab66ed1
Shodai Suzuki <info@soartec-lab.work>
2024-08-03 21:09:35 +0900

    chore: add sample app for `swr` with `fetch` as an http client (#1561)

---
+++ samples/react-app-with-swr/fetch-client/orval.config.ts
@@ -0,0 +1,19 @@
+import { faker } from '@faker-js/faker';
+import { defineConfig } from 'orval';
+
+export default defineConfig({
+  petstore: {
+    output: {
+      baseUrl: 'http://localhost:8000',
+      mode: 'split',
+      target: 'src/api/endpoints',
+      schemas: 'src/api/models',
+      client: 'swr',
+      httpClient: 'fetch',
+      mock: true,
+    },
+    input: {
+      target: './petstore.yaml',
+    },
+  },
+});
