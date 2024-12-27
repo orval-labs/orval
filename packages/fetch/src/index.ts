c89d9c9b88eb796133972bd112752cf9f19d803b
Alice Jonsson <10475857+AllieJonsson@users.noreply.github.com>
2024-12-27 01:47:13 +0100

    fix(fetch): make sure to not double promise for custom fetch mutator (#1768)

--- packages/fetch/src/index.ts
+++ packages/fetch/src/index.ts
@@ -139,7 +139,7 @@ ${
     .join(',');
 
   const args = `${toObjectString(props, 'implementation')} ${isRequestOptions ? `options?: RequestInit` : ''}`;
-  const retrunType = `Promise<${responseTypeName}>`;
+  const returnType = `Promise<${responseTypeName}>`;
 
   const globalFetchOptions = isObject(override?.requestOptions)
     ? `${stringify(override?.requestOptions)?.slice(1, -1)?.trim()}`
@@ -180,7 +180,7 @@ ${
 
   ${override.fetch.includeHttpResponseReturnType ? 'return { status: res.status, data, headers: res.headers }' : `return data as ${responseTypeName}`}
 `;
-  const customFetchResponseImplementation = `return ${mutator?.name}<${retrunType}>(${fetchFnOptions});`;
+  const customFetchResponseImplementation = `return ${mutator?.name}<${responseTypeName}>(${fetchFnOptions});`;
 
   const bodyForm = generateFormDataAndUrlEncodedFunction({
     formData,
@@ -194,7 +194,7 @@ ${
     ? customFetchResponseImplementation
     : fetchResponseImplementation;
 
-  const fetchImplementation = `export const ${operationName} = async (${args}): ${retrunType} => {
+  const fetchImplementation = `export const ${operationName} = async (${args}): ${returnType} => {
   ${bodyForm ? `  ${bodyForm}` : ''}
   ${fetchImplementationBody}}
 `;
