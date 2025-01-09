63b90692ea3d014cee07a10addc18d06f7a227c4
Victor Bury <victor@emaniax.io>
2025-01-09 12:19:18 +0100

    fix(query): infinite query and placeholder type and new datatag error

---
+++ tests/regressions/react-query.ts
@@ -0,0 +1,41 @@
+import { keepPreviousData } from '@tanstack/react-query';
+import {
+  useListPets,
+  useListPetsInfinite,
+} from '../generated/react-query/mutator/endpoints';
+
+export const useInfiniteQueryTest = () => {
+  const { data } = useListPetsInfinite(
+    {
+      sort: 'name',
+    },
+    0,
+    {
+      query: {
+        getNextPageParam: (lastPage) => lastPage[0].id.toString(),
+      },
+    },
+  );
+
+  const pages = data?.pages.map((page) => page.map((pet) => pet.name)).flat();
+
+  return pages;
+};
+
+export const useHookWithPlaceHolderData = () => {
+  const { data } = useListPets(
+    {
+      sort: 'name',
+    },
+    0,
+    {
+      query: {
+        placeholderData: keepPreviousData,
+      },
+    },
+  );
+
+  const names = data?.map((pet) => pet.name);
+
+  return names;
+};
