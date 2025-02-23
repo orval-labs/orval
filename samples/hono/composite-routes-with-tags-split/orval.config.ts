11356df72986b78e45c1cbeafe38dd8a4b08a10b
soartec-lab <info@soartec-lab.work>
2025-02-23 04:21:05 +0000

    chore: add `hono` sample app for using `compositRoutes`

---
+++ samples/hono/composite-routes-with-tags-split/orval.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'orval';
+
+export default defineConfig({
+  api: {
+    input: {
+      target: './petstore.yaml',
+    },
+    output: {
+      mode: 'tags-split',
+      client: 'hono',
+      target: 'src/endpoints',
+      schemas: 'src/schemas',
+      override: {
+        hono: {
+          compositeRoute: 'src/routes.ts',
+          validatorOutputPath: 'src/endpoints/validator.ts',
+        },
+      },
+    },
+  },
+});
