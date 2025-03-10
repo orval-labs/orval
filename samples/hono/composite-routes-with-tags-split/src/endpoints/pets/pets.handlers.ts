8682e6272a649b4786288a216dcb2a805b6cc77e
melloware <mellowaredev@gmail.com>
2025-03-10 07:39:52 -0400

    chore(deps): fix jsonpath-plus

--- samples/hono/composite-routes-with-tags-split/src/endpoints/pets/pets.handlers.ts
+++ samples/hono/composite-routes-with-tags-split/src/endpoints/pets/pets.handlers.ts
@@ -1,40 +1,46 @@
 import { createFactory } from 'hono/factory';
 import { zValidator } from '../validator';
+import { ListPetsContext,
+CreatePetsContext,
+UpdatePetsContext,
+ShowPetByIdContext } from './pets.context';
 import {
-  ListPetsContext,
-  CreatePetsContext,
-  UpdatePetsContext,
-  ShowPetByIdContext,
-} from './pets.context';
-import {
-  listPetsQueryParams,
-  listPetsResponse,
-  createPetsBody,
-  createPetsResponse,
-  updatePetsBody,
-  updatePetsResponse,
-  showPetByIdParams,
-  showPetByIdResponse,
-} from './pets.zod';
+listPetsQueryParams,
+listPetsResponse,
+createPetsBody,
+createPetsResponse,
+updatePetsBody,
+updatePetsResponse,
+showPetByIdParams,
+showPetByIdResponse
+} from './pets.zod'
 
 const factory = createFactory();
 export const listPetsHandlers = factory.createHandlers(
-  zValidator('query', listPetsQueryParams),
-  zValidator('response', listPetsResponse),
-  async (c: ListPetsContext) => {},
+zValidator('query', listPetsQueryParams),
+zValidator('response', listPetsResponse),
+async (c: ListPetsContext) => {
+
+  },
 );
 export const createPetsHandlers = factory.createHandlers(
-  zValidator('json', createPetsBody),
-  zValidator('response', createPetsResponse),
-  async (c: CreatePetsContext) => {},
+zValidator('json', createPetsBody),
+zValidator('response', createPetsResponse),
+async (c: CreatePetsContext) => {
+
+  },
 );
 export const updatePetsHandlers = factory.createHandlers(
-  zValidator('json', updatePetsBody),
-  zValidator('response', updatePetsResponse),
-  async (c: UpdatePetsContext) => {},
+zValidator('json', updatePetsBody),
+zValidator('response', updatePetsResponse),
+async (c: UpdatePetsContext) => {
+
+  },
 );
 export const showPetByIdHandlers = factory.createHandlers(
-  zValidator('param', showPetByIdParams),
-  zValidator('response', showPetByIdResponse),
-  async (c: ShowPetByIdContext) => {},
+zValidator('param', showPetByIdParams),
+zValidator('response', showPetByIdResponse),
+async (c: ShowPetByIdContext) => {
+
+  },
 );
