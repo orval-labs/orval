55e36b66276eb5f5fdc1ce4e573c7ab57169643b
Sampsa Kaskela <sampsa.kaskela@gmail.com>
2025-05-31 20:20:57 +0300

    fix: use minItems and maxItems when mocking arrays (#2131)

--- packages/mock/src/msw/mocks.ts
+++ packages/mock/src/msw/mocks.ts
@@ -97,15 +97,15 @@ const getMockScalarJsTypes = (
     case 'number':
       return isArray
         ? `Array.from({length: faker.number.int({` +
-            `min: ${mockOptionsWithoutFunc.arrayMin}, ` +
-            `max: ${mockOptionsWithoutFunc.arrayMax}}` +
+            `min: ${mockOptionsWithoutFunc.arrayMin ?? 1}, ` +
+            `max: ${mockOptionsWithoutFunc.arrayMax ?? 10}}` +
             `)}, () => faker.number.int())`
         : 'faker.number.int()';
     case 'string':
       return isArray
         ? `Array.from({length: faker.number.int({` +
-            `min: ${mockOptionsWithoutFunc?.arrayMin},` +
-            `max: ${mockOptionsWithoutFunc?.arrayMax}}` +
+            `min: ${mockOptionsWithoutFunc?.arrayMin ?? 1},` +
+            `max: ${mockOptionsWithoutFunc?.arrayMax ?? 10}}` +
             `)}, () => faker.word.sample())`
         : 'faker.word.sample()';
     default:
