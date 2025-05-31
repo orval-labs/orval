ddbc4122554177dd259df7e44d6969ba5bd946cd
Sampsa Kaskela <sampsa.kaskela@gmail.com>
2025-05-31 18:38:58 +0300

    fix: fix tests

--- packages/mock/src/faker/getters/scalar.ts
+++ packages/mock/src/faker/getters/scalar.ts
@@ -236,7 +236,7 @@ export const getMockScalar = ({
     case 'string': {
       const length =
         item.minLength !== undefined || item.maxLength !== undefined
-          ? `{length: {min: ${item.minLength ?? 0}, max: ${item.maxLength ?? (item.minLength ?? 0) + 20}}`
+          ? `{length: {min: ${item.minLength ?? 0}, max: ${item.maxLength ?? (item.minLength ?? 0) + 20}}}`
           : 20;
       let value = `faker.string.alpha(${length})`;
       const stringImports: GeneratorImport[] = [];
