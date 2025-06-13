ac405929792c41c264049fae2ad56a117f7a0b41
Chris Dascoli <chris.dascoli@fountain.com>
2025-06-13 16:23:27 -0600

    feat: 2163 mcp handlers accept options

--- packages/mcp/src/index.ts
+++ packages/mcp/src/index.ts
@@ -131,6 +131,8 @@ export const generateMcp: ClientBuilder = (verbOptions) => {
     handlerArgsTypes.push(`  bodyParams: ${verbOptions.body.definition};`);
   }
 
+  handlerArgsTypes.push(`  options?: RequestInit;`);
+
   const handlerArgsName = `${verbOptions.operationName}Args`;
   const handlerArgsImplementation =
     handlerArgsTypes.length > 0
@@ -151,6 +153,8 @@ ${handlerArgsTypes.join('\n')}
   }
   if (verbOptions.body.definition) fetchParams.push(`args.bodyParams`);
   if (verbOptions.queryParams) fetchParams.push(`args.queryParams`);
+
+  fetchParams.push(`args.options`);
 
   const handlerName = `${verbOptions.operationName}Handler`;
   const handlerImplementation = `
