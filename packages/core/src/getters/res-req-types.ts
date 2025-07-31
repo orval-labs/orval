9e3a06f98f545b6179f8e0239151f2c13bf9e4c6
Arthur Fiorette <47537704+arthurfiorette@users.noreply.github.com>
2025-07-31 17:35:56 -0300

    fix: ensure import alias is only used when imports are needed (#2257)
    * fix: ensure import alias is only used when imports are needed
    
    * yarn lock
    
    * better comment

--- packages/core/src/getters/res-req-types.ts
+++ packages/core/src/getters/res-req-types.ts
@@ -311,14 +311,17 @@ const getSchemaFormDataAndUrlEncoded = ({
             context,
           );
 
-          if (shouldCast) additionalImports.push(imports[0]);
+          let newPropName = propName;
+          let newPropDefinition = '';
 
-          const newPropName = shouldCast
-            ? `${propName}${pascal(imports[0].name)}`
-            : propName;
-          const newPropDefinition = shouldCast
-            ? `const ${newPropName} = (${propName} as ${imports[0].name}${isRequestBodyOptional ? ' | undefined' : ''});\n`
-            : '';
+          // If the schema is a union type (oneOf, anyOf) and includes a reference (has imports),
+          // we need to cast the property to the specific type to avoid TypeScript errors.
+          if (shouldCast && imports[0]) {
+            additionalImports.push(imports[0]);
+            newPropName = `${propName}${pascal(imports[0].name)}`;
+            newPropDefinition = `const ${newPropName} = (${propName} as ${imports[0].name}${isRequestBodyOptional ? ' | undefined' : ''});\n`;
+          }
+
           return (
             newPropDefinition +
             resolveSchemaPropertiesToFormData({
