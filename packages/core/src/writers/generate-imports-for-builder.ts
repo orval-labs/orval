ed91d67bf18ce2cf2e0873808c9ae4a9151cd0f0
ung0v <vuong.pw@gmail.com>
2025-08-20 08:41:30 +0700

    fix: check name by conventionName

--- packages/core/src/writers/generate-imports-for-builder.ts
+++ packages/core/src/writers/generate-imports-for-builder.ts
@@ -1,15 +1,19 @@
 import uniqBy from 'lodash.uniqby';
 import { GeneratorImport, NormalizedOutputOptions } from '../types';
-import { upath, camel } from '../utils';
+import { conventionName, upath } from '../utils';
 
 export const generateImportsForBuilder = (
   output: NormalizedOutputOptions,
   imports: GeneratorImport[],
   relativeSchemasPath: string,
-) =>
-  output.schemas && !output.indexFiles
-    ? uniqBy(imports, 'name').map((i) => ({
-        exports: [i],
-        dependency: upath.joinSafe(relativeSchemasPath, i.name),
-      }))
+) => {
+  return output.schemas && !output.indexFiles
+    ? uniqBy(imports, 'name').map((i) => {
+        const name = conventionName(i.name, output.namingConvention);
+        return {
+          exports: [i],
+          dependency: upath.joinSafe(relativeSchemasPath, name),
+        };
+      })
     : [{ exports: imports, dependency: relativeSchemasPath }];
+};
