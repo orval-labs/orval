c2eb460fc9e9b9f49973c42f00c020e729fed633
snebjorn <eskildd@gmail.com>
2025-08-20 22:41:10 +0200

    feat: switch to exports field in package.json

--- packages/tsup.base.ts
+++ packages/tsup.base.ts
@@ -2,7 +2,7 @@ import { Options } from 'tsup';
 
 export const baseOptions = {
   entry: ['src/index.ts'],
-  target: 'node12',
+  target: 'node18',
   sourcemap: true,
   clean: true,
 } satisfies Options;
