fa8dd6c5d74da265f32c6cc16062fa17faeb43f3
snebjorn <eskildd@gmail.com>
2025-08-25 22:11:09 +0200

    chore: enables goto definition for the packages inside the mono-repo

--- packages/tsup.base.ts
+++ packages/tsup.base.ts
@@ -3,6 +3,8 @@ import { Options } from 'tsup';
 export const baseOptions = {
   entry: ['src/index.ts'],
   target: 'node18',
-  sourcemap: true,
+  sourcemap: false,
   clean: true,
+  // https://tsup.egoist.dev/#generate-typescript-declaration-maps--d-ts-map
+  onSuccess: 'tsc --emitDeclarationOnly --declaration',
 } satisfies Options;
