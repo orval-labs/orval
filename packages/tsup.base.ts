bbbd46e9157019a96120ce9e9038b856340d86d2
snebjorn <eskildd@gmail.com>
2025-09-13 18:38:05 +0200

    chore: fixes eslint complaining about files not being covered by tsconfig

--- packages/tsup.base.ts
+++ packages/tsup.base.ts
@@ -1,10 +1,12 @@
 import { Options } from 'tsup';
 
+const tsconfig = 'tsconfig.build.json';
 export const baseOptions = {
   entry: ['src/index.ts'],
   target: 'node18',
+  tsconfig: tsconfig,
   sourcemap: false,
   clean: true,
   // https://tsup.egoist.dev/#generate-typescript-declaration-maps--d-ts-map
-  onSuccess: 'tsc --emitDeclarationOnly --declaration',
+  onSuccess: `tsc --project ${tsconfig} --emitDeclarationOnly --declaration`,
 } satisfies Options;
