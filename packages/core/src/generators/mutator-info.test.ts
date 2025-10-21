4f301cfacf63b97320e79f28652c6296d406cb6e
snebjorn <eskildd@gmail.com>
2025-10-21 22:54:36 +0200

    fixes odd path issue on CI

--- packages/core/src/generators/mutator-info.test.ts
+++ packages/core/src/generators/mutator-info.test.ts
@@ -1,3 +1,5 @@
+import path from 'node:path';
+
 import { describe, expect, it } from 'vitest';
 
 import {
@@ -18,90 +20,90 @@ import {
 } from './__tests__/mutator-test-files/named-export-tests';
 import { getMutatorInfo } from './mutator-info';
 
-const basePath = 'packages/core/src/generators/__tests__/mutator-test-files';
+const basePath = path.join(import.meta.dirname, '__tests__/mutator-test-files');
 
 describe('getMutatorInfo', () => {
   describe('default anonymous export', () => {
     it('should work for anonymous function with 0 args', async () => {
       const result = await getMutatorInfo(
-        `${basePath}/default-anonymous-function-0-args.ts`,
+        path.join(basePath, 'default-anonymous-function-0-args.ts'),
       );
       expect(result).toEqual({ numberOfParams: 0 });
     });
 
     it('should work for anonymous function with 1 args', async () => {
       const result = await getMutatorInfo(
-        `${basePath}/default-anonymous-function-1-args.ts`,
+        path.join(basePath, 'default-anonymous-function-1-args.ts'),
       );
       expect(result).toEqual({ numberOfParams: 1 });
     });
 
     it('should work for anonymous function with 2 args', async () => {
       const result = await getMutatorInfo(
-        `${basePath}/default-anonymous-function-2-args.ts`,
+        path.join(basePath, 'default-anonymous-function-2-args.ts'),
       );
       expect(result).toEqual({ numberOfParams: 2 });
     });
 
     it('should work for anonymous function with 3 args', async () => {
       const result = await getMutatorInfo(
-        `${basePath}/default-anonymous-function-3-args.ts`,
+        path.join(basePath, 'default-anonymous-function-3-args.ts'),
       );
       expect(result).toEqual({ numberOfParams: 3 });
     });
 
     it('should work for anonymous lambda with 0 args', async () => {
       const result = await getMutatorInfo(
-        `${basePath}/default-anonymous-lambda-0-args.ts`,
+        path.join(basePath, 'default-anonymous-lambda-0-args.ts'),
       );
       expect(result).toEqual({ numberOfParams: 0 });
     });
 
     it('should work for anonymous lambda with 1 args', async () => {
       const result = await getMutatorInfo(
-        `${basePath}/default-anonymous-lambda-1-args.ts`,
+        path.join(basePath, 'default-anonymous-lambda-1-args.ts'),
       );
       expect(result).toEqual({ numberOfParams: 1 });
     });
 
     it('should work for anonymous lambda with 2 args', async () => {
       const result = await getMutatorInfo(
-        `${basePath}/default-anonymous-lambda-2-args.ts`,
+        path.join(basePath, 'default-anonymous-lambda-2-args.ts'),
       );
       expect(result).toEqual({ numberOfParams: 2 });
     });
 
     it('should work for anonymous lambda with 3 args', async () => {
       const result = await getMutatorInfo(
-        `${basePath}/default-anonymous-lambda-3-args.ts`,
+        path.join(basePath, 'default-anonymous-lambda-3-args.ts'),
       );
       expect(result).toEqual({ numberOfParams: 3 });
     });
 
     it('should work for anonymous lambda returning lambda with 0 args', async () => {
       const result = await getMutatorInfo(
-        `${basePath}/default-anonymous-nested-lambda-0-args.ts`,
+        path.join(basePath, 'default-anonymous-nested-lambda-0-args.ts'),
       );
       expect(result).toEqual({ numberOfParams: 0, returnNumberOfParams: 0 });
     });
 
     it('should work for anonymous lambda returning lambda with 1 args', async () => {
       const result = await getMutatorInfo(
-        `${basePath}/default-anonymous-nested-lambda-1-args.ts`,
+        path.join(basePath, 'default-anonymous-nested-lambda-1-args.ts'),
       );
       expect(result).toEqual({ numberOfParams: 0, returnNumberOfParams: 1 });
     });
 
     it('should work for anonymous lambda returning lambda with 2 args', async () => {
       const result = await getMutatorInfo(
-        `${basePath}/default-anonymous-nested-lambda-2-args.ts`,
+        path.join(basePath, 'default-anonymous-nested-lambda-2-args.ts'),
       );
       expect(result).toEqual({ numberOfParams: 0, returnNumberOfParams: 2 });
     });
 
     it('should work for anonymous lambda returning lambda with 3 args', async () => {
       const result = await getMutatorInfo(
-        `${basePath}/default-anonymous-nested-lambda-3-args.ts`,
+        path.join(basePath, 'default-anonymous-nested-lambda-3-args.ts'),
       );
       expect(result).toEqual({ numberOfParams: 0, returnNumberOfParams: 3 });
     });
@@ -109,100 +111,142 @@ describe('getMutatorInfo', () => {
 
   describe('named export', () => {
     it('should work for function with 0 args', async () => {
-      const result = await getMutatorInfo(`${basePath}/named-export-tests.ts`, {
-        namedExport: fn0Param.name,
-      });
+      const result = await getMutatorInfo(
+        path.join(basePath, 'named-export-tests.ts'),
+        {
+          namedExport: fn0Param.name,
+        },
+      );
       expect(result).toEqual({ numberOfParams: 0 });
     });
 
     it('should work for function with 1 args', async () => {
-      const result = await getMutatorInfo(`${basePath}/named-export-tests.ts`, {
-        namedExport: fn1Param.name,
-      });
+      const result = await getMutatorInfo(
+        path.join(basePath, 'named-export-tests.ts'),
+        {
+          namedExport: fn1Param.name,
+        },
+      );
       expect(result).toEqual({ numberOfParams: 1 });
     });
 
     it('should work for function with 2 args', async () => {
-      const result = await getMutatorInfo(`${basePath}/named-export-tests.ts`, {
-        namedExport: fn2Param.name,
-      });
+      const result = await getMutatorInfo(
+        path.join(basePath, 'named-export-tests.ts'),
+        {
+          namedExport: fn2Param.name,
+        },
+      );
       expect(result).toEqual({ numberOfParams: 2 });
     });
 
     it('should work for function with 3 args', async () => {
-      const result = await getMutatorInfo(`${basePath}/named-export-tests.ts`, {
-        namedExport: fn3Param.name,
-      });
+      const result = await getMutatorInfo(
+        path.join(basePath, 'named-export-tests.ts'),
+        {
+          namedExport: fn3Param.name,
+        },
+      );
       expect(result).toEqual({ numberOfParams: 3 });
     });
 
     it('should work for lambda with 0 args', async () => {
-      const result = await getMutatorInfo(`${basePath}/named-export-tests.ts`, {
-        namedExport: lambda0Param.name,
-      });
+      const result = await getMutatorInfo(
+        path.join(basePath, 'named-export-tests.ts'),
+        {
+          namedExport: lambda0Param.name,
+        },
+      );
       expect(result).toEqual({ numberOfParams: 0 });
     });
 
     it('should work for lambda with 1 args', async () => {
-      const result = await getMutatorInfo(`${basePath}/named-export-tests.ts`, {
-        namedExport: lambda1Param.name,
-      });
+      const result = await getMutatorInfo(
+        path.join(basePath, 'named-export-tests.ts'),
+        {
+          namedExport: lambda1Param.name,
+        },
+      );
       expect(result).toEqual({ numberOfParams: 1 });
     });
 
     it('should work for lambda with 2 args', async () => {
-      const result = await getMutatorInfo(`${basePath}/named-export-tests.ts`, {
-        namedExport: lambda2Param.name,
-      });
+      const result = await getMutatorInfo(
+        path.join(basePath, 'named-export-tests.ts'),
+        {
+          namedExport: lambda2Param.name,
+        },
+      );
       expect(result).toEqual({ numberOfParams: 2 });
     });
 
     it('should work for lambda with 3 args', async () => {
-      const result = await getMutatorInfo(`${basePath}/named-export-tests.ts`, {
-        namedExport: lambda3Param.name,
-      });
+      const result = await getMutatorInfo(
+        path.join(basePath, 'named-export-tests.ts'),
+        {
+          namedExport: lambda3Param.name,
+        },
+      );
       expect(result).toEqual({ numberOfParams: 3 });
     });
 
     it('should work for lambda returning lambda with 0 args', async () => {
-      const result = await getMutatorInfo(`${basePath}/named-export-tests.ts`, {
-        namedExport: nestedLambda0Param.name,
-      });
+      const result = await getMutatorInfo(
+        path.join(basePath, 'named-export-tests.ts'),
+        {
+          namedExport: nestedLambda0Param.name,
+        },
+      );
       expect(result).toEqual({ numberOfParams: 0, returnNumberOfParams: 0 });
     });
 
     it('should work for lambda returning lambda with 1 args', async () => {
-      const result = await getMutatorInfo(`${basePath}/named-export-tests.ts`, {
-        namedExport: nestedLambda1Param.name,
-      });
+      const result = await getMutatorInfo(
+        path.join(basePath, 'named-export-tests.ts'),
+        {
+          namedExport: nestedLambda1Param.name,
+        },
+      );
       expect(result).toEqual({ numberOfParams: 0, returnNumberOfParams: 1 });
     });
 
     it('should work for lambda returning lambda with 2 args', async () => {
-      const result = await getMutatorInfo(`${basePath}/named-export-tests.ts`, {
-        namedExport: nestedLambda2Param.name,
-      });
+      const result = await getMutatorInfo(
+        path.join(basePath, 'named-export-tests.ts'),
+        {
+          namedExport: nestedLambda2Param.name,
+        },
+      );
       expect(result).toEqual({ numberOfParams: 0, returnNumberOfParams: 2 });
     });
 
     it('should work for lambda returning lambda with 3 args', async () => {
-      const result = await getMutatorInfo(`${basePath}/named-export-tests.ts`, {
-        namedExport: nestedLambda3Param.name,
-      });
+      const result = await getMutatorInfo(
+        path.join(basePath, 'named-export-tests.ts'),
+        {
+          namedExport: nestedLambda3Param.name,
+        },
+      );
       expect(result).toEqual({ numberOfParams: 0, returnNumberOfParams: 3 });
     });
 
     it('should work for callback function with 2 args', async () => {
-      const result = await getMutatorInfo(`${basePath}/named-export-tests.ts`, {
-        namedExport: fnCallback.name,
-      });
+      const result = await getMutatorInfo(
+        path.join(basePath, 'named-export-tests.ts'),
+        {
+          namedExport: fnCallback.name,
+        },
+      );
       expect(result).toEqual({ numberOfParams: 2 });
     });
 
     it('should work for function returning lambda with 2 args', async () => {
-      const result = await getMutatorInfo(`${basePath}/named-export-tests.ts`, {
-        namedExport: fnNestedLambda2Param.name,
-      });
+      const result = await getMutatorInfo(
+        path.join(basePath, 'named-export-tests.ts'),
+        {
+          namedExport: fnNestedLambda2Param.name,
+        },
+      );
       expect(result).toEqual({ numberOfParams: 0, returnNumberOfParams: 2 });
     });
   });
