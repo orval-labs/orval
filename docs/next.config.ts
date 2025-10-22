6efa7840925bf14e4616d10210fae7ec78653559
snebjorn <eskildd@gmail.com>
2025-10-23 01:01:59 +0200

    updated to next@16

--- docs/next.config.ts
+++ docs/next.config.ts
@@ -1,4 +1,3 @@
-import process from 'node:process';
 import type { NextConfig } from 'next';
 import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings, {
@@ -13,10 +12,27 @@ import remarkFrontmatter from 'remark-frontmatter';
 import remarkMdxFrontmatter, {
   RemarkMdxFrontmatterOptions,
 } from 'remark-mdx-frontmatter';
-import nextMdx from '@next/mdx';
+import createMDX from '@next/mdx';
 import recmaNextjsStaticProps from 'recma-nextjs-static-props';
 
-const withMdx = nextMdx({
+const nextConfig: NextConfig = {
+  transpilePackages: ['orval'],
+  experimental: {
+    // fixes:
+    // Module not found: ESM packages (chalk) need to be imported. Use 'import' to reference the package instead. https://nextjs.org/docs/messages/import-esm-externals
+    esmExternals: 'loose',
+  },
+  // Support MDX files as pages:
+  pageExtensions: ['md', 'mdx', 'tsx', 'ts', 'jsx', 'js'],
+  rewrites: async () => [
+    {
+      source: '/docs{/}?',
+      destination: '/docs/overview',
+    },
+  ],
+};
+
+const withMdx = createMDX({
   // By default only the `.mdx` extension is supported.
   extension: /\.mdx?$/,
   options: {
@@ -50,36 +66,5 @@ const withMdx = nextMdx({
     ],
   },
 });
-
-const nextConfig: NextConfig = {
-  transpilePackages: ['orval'],
-  experimental: {
-    // fixes:
-    // Module not found: ESM packages (chalk) need to be imported. Use 'import' to reference the package instead. https://nextjs.org/docs/messages/import-esm-externals
-    esmExternals: 'loose',
-  },
-  // Support MDX files as pages:
-  pageExtensions: ['md', 'mdx', 'tsx', 'ts', 'jsx', 'js'],
-  rewrites: async () => [
-    {
-      source: '/docs{/}?',
-      destination: '/docs/overview',
-    },
-  ],
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && isServer) {
-      // we're in build mode so enable shared caching for the GitHub API
-      process.env.USE_CACHE = 'true';
-      const originalEntry = config.entry;
-
-      config.entry = async () => {
-        const entries = { ...(await originalEntry()) };
-        return entries;
-      };
-    }
-
-    return config;
-  },
-};
 
 export default withMdx(nextConfig);
