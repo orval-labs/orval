af5f1d5a3f92d80d1732c010fa2d43c806520f7c
Eric Tucker <tucker.ericm@gmail.com>
2025-10-29 11:57:14 -0700

    feat: allow mts config file extension (#2509)

--- packages/orval/src/generate.ts
+++ packages/orval/src/generate.ts
@@ -102,7 +102,7 @@ function findConfigFile(configFilePath?: string) {
   }
 
   const root = process.cwd();
-  const exts = ['.ts', '.js', '.mjs'];
+  const exts = ['.ts', '.js', '.mjs', '.mts'];
   for (const ext of exts) {
     const fullPath = path.resolve(root, `orval.config${ext}`);
     if (fs.existsSync(fullPath)) {
