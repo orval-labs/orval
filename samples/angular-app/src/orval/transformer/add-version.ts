cb66a2e72fc909b8d39d2976da4f76c816fa3c95
snebjorn <eskildd@gmail.com>
2025-12-06 16:54:59 +0100

    WIP

--- samples/angular-app/src/orval/transformer/add-version.ts
+++ samples/angular-app/src/orval/transformer/add-version.ts
@@ -2,10 +2,10 @@ import type { InputTransformerFn } from 'orval';
 
 const transformer: InputTransformerFn = (inputSchema) => ({
   ...inputSchema,
-  paths: Object.entries(inputSchema.paths).reduce(
+  paths: Object.entries(inputSchema.paths ?? {}).reduce(
     (acc, [path, pathItem]) => ({
       ...acc,
-      [`/v{version}${path}`]: Object.entries(pathItem).reduce(
+      [`/v{version}${path}`]: Object.entries(pathItem ?? {}).reduce(
         (pathItemAcc, [verb, operation]) => ({
           ...pathItemAcc,
           [verb]: {
