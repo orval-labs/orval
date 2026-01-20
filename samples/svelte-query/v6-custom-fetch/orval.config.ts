4acdaedf356b312ab38c63efa9081927a53318ca
Alice Jonsson <10475857+AllieJonsson@users.noreply.github.com>
2026-01-20 16:24:41 +0100

    tests(svelte): add svelte-query v6 sample

---
+++ samples/svelte-query/v6-custom-fetch/orval.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'orval';
+
+export default defineConfig({
+  petstore: {
+    output: {
+      mode: 'tags-split',
+      target: 'src/gen/petstore.ts',
+      schemas: 'src/gen/models',
+      client: 'svelte-query',
+      baseUrl: 'http://localhost:8000',
+      mock: true,
+      prettier: true,
+      override: {
+        mutator: {
+          path: './src/custom-fetch.ts',
+          name: 'customFetch',
+        },
+      },
+    },
+    input: {
+      target: './petstore.yaml',
+    },
+  },
+});
