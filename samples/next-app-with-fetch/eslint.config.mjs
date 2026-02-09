f8e8f6712ce0fb6787a589cf29879d89710fd185
The Ult <ult_dev@pm.me>
2026-02-09 20:31:43 +0100

    chore: revert out of scope package updates

--- samples/next-app-with-fetch/eslint.config.mjs
+++
@@ -1,9 +0,0 @@
-import { FlatCompat } from '@eslint/eslintrc';
-import js from '@eslint/js';
-
-const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
-  recommendedConfig: js.configs.recommended,
-});
-
-export default [...compat.extends('next/core-web-vitals')];
