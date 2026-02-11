eef9914c8b64cc5260287477207f63fcbc74818b
arcalise08 <arcalise08@gmail.com>
2026-02-11 10:50:12 -0600

    updated sample to better showcase url handling

--- samples/react-query/hook-mutator-fetch/use-custom-instance.ts
+++ samples/react-query/hook-mutator-fetch/use-custom-instance.ts
@@ -1,10 +1,16 @@
-import { useCallback } from 'react';
+import { useCallback, useState } from 'react';
+
+export function useGetBaseUrl() {
+  const [baseUrl] = useState('')
+  return baseUrl;
+}
 
 export function useCustomFetch() {
+  const baseUrl = useGetBaseUrl();
   return useCallback(
     async <T>(url: string, options?: RequestInit): Promise<T> => {
       const headers = new Headers(options?.headers);
-      const response = await fetch(url, {
+      const response = await fetch(`${baseUrl}${url}`, {
         ...options,
         headers,
       });
