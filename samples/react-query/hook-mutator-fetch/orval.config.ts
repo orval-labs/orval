edd174cc0971eca257f6af0839c6baae051ae55c
Arcalise08 <61042848+Arcalise08@users.noreply.github.com>
2026-02-16 12:42:17 -0600

    Fixed issue when using react hooks with httpClient set to "fetch" (#2943)
    * feat/fix/chore/docs: Fixed issue when using react style hooks with fetch
    
    * feat/fix/chore/docs: Fixed issue with formData in fetch hook
    
    * removed axios from hook-mutator-fetch sample
    
    * updated sample to better showcase url handling
    
    * updated the package.json of both projects to match recent changes
    
    * Updated yarn.lock for build
    
    * Updated samples for build
    
    * everything passing
    
    ---------
    
    Co-authored-by: Melloware <mellowaredev@gmail.com>

---
+++ samples/react-query/hook-mutator-fetch/orval.config.ts
@@ -0,0 +1,49 @@
+import { defineConfig } from 'orval';
+
+export default defineConfig({
+  petstore: {
+    output: {
+      target: './endpoints.ts',
+      schemas: './models',
+      client: 'react-query',
+      httpClient: 'fetch',
+      prettier: true,
+      override: {
+        mutator: {
+          path: './use-custom-instance.ts',
+          name: 'useCustomFetch',
+        },
+
+        // only for the operation id 'createPets'
+        // operations: {
+        //   createPets: {
+        //     mutator: {
+        //       path: './use-custom-instance.ts',
+        //       name: 'useCustomInstance',
+        //     },
+        //   },
+        // },
+        // only for the tag 'pets'
+        // tags: {
+        //   pets: {
+        //     mutator: {
+        //       path: './use-custom-instance.ts',
+        //       name: 'useCustomInstance',
+        //     },
+        //   },
+        // },
+        // for ES Module imports
+        // {
+        //  mutator: {
+        //    path: './use-custom-instance.ts',
+        //    name: 'useCustomInstance',
+        //    extension: '.js',
+        //  }
+        // }
+      },
+    },
+    input: {
+      target: './petstore.yaml',
+    },
+  },
+});
