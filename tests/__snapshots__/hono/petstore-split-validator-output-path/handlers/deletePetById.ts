8b9f26bd20c78c739664517625281f96497ca56f
Victor Bury <victor@emaniax.io>
2026-03-06 23:54:55 +0100

    fix(snapshots): update

---
+++ tests/__snapshots__/hono/petstore-split-validator-output-path/handlers/deletePetById.ts
@@ -0,0 +1,16 @@
+import { createFactory } from 'hono/factory';
+import { zValidator } from './validator';
+import { DeletePetByIdContext } from '../endpoints.context';
+import {
+deletePetByIdParams
+} from '../endpoints.zod'
+
+const factory = createFactory();
+
+
+export const deletePetByIdHandlers = factory.createHandlers(
+zValidator('param', deletePetByIdParams),
+async (c: DeletePetByIdContext) => {
+
+  },
+);
