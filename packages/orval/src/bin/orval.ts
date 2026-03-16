0e3e35f5dff41248fe48908b718271218cf6d902
Ivan Križnar <hyperNURb@users.noreply.github.com>
2026-03-16 13:37:16 +0100

    Update packages/orval/src/bin/orval.ts
    Co-authored-by: Eskild Diderichsen <eskildd@gmail.com>

--- packages/orval/src/bin/orval.ts
+++ packages/orval/src/bin/orval.ts
@@ -150,9 +150,7 @@ cli
 
       let hasErrors = false;
       for (const [projectName, config] of configs) {
-        const normalizedOptions = await normalizeOptions(config, workspace, {
-          ...options,
-        });
+        const normalizedOptions = await normalizeOptions(config, workspace, options);
 
         if (options.watch === undefined) {
           try {
