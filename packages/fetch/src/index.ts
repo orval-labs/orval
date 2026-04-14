bfbad47843d13b30a03c982bef47ebcc261bfe9f
Tarvion <31004928+Tarvion@users.noreply.github.com>
2026-04-14 13:58:21 +0200

    only add schema parameter if validation is enabled

--- packages/fetch/src/index.ts
+++ packages/fetch/src/index.ts
@@ -560,7 +560,7 @@ ${override.fetch.forceSuccessResponse && hasSuccess ? '' : `export type ${respon
   const schemaValueRef =
     responseType === 'Error' ? 'ErrorSchema' : responseType;
   const validateFetchFnOptions = `${rawFetchFnOptions}${isValidateResponse ? ',' : ''}
-    schema: ${schemaValueRef}
+    ${isValidateResponse ? `schema: ${schemaValueRef}` : ''}
   }    
 `;
   const reviver = fetchReviver ? `, ${fetchReviver.name}` : '';
