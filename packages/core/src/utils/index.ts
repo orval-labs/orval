e6c2822051107b96aeea64eab21f99e0135f87ff
wadakatu <72595463+wadakatu@users.noreply.github.com>
2026-09-19 19:47:02 +0900

    refactor(core): remove unused count helper (#4150)

--- packages/core/src/utils/index.ts
+++ packages/core/src/utils/index.ts
@@ -16,7 +16,6 @@ export * from './is-body-verb';
 export * from './logger';
 export * from './merge-deep';
 export * from './name';
-export * from './occurrence';
 export * as upath from './path';
 export * from './required';
 export * from './resolve-version';
