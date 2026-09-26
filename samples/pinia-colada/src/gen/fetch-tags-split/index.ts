2522b7a2fa531b4b13f5f1f5e4f6bdd3a151647a
snebjorn <eskildd@gmail.com>
2026-09-26 12:43:50 +0200

    include ignored generated output

---
+++ samples/pinia-colada/src/gen/fetch-tags-split/index.ts
@@ -0,0 +1,2 @@
+export * from './default/default';
+export * from './pets/pets';
---
+++ samples/solid-query/custom-fetch/src/gen/index.ts
@@ -0,0 +1,7 @@
+export type { HTTPStatusCode1xx } from './pets/pets.ts';
+export type { HTTPStatusCode2xx } from './pets/pets.ts';
+export type { HTTPStatusCode3xx } from './pets/pets.ts';
+export type { HTTPStatusCode4xx } from './pets/pets.ts';
+export type { HTTPStatusCode5xx } from './pets/pets.ts';
+export type { HTTPStatusCodes } from './pets/pets.ts';
+export * from './pets/pets.ts';
